5e7b82e09f716853382af7ae85d021316fd5cc1e
frank <frank@bean.com>
2021-01-27 12:17:20 -0800

    Adding an IModule

---
+++ web-xxe/web-xxe-server/_bin/fixlocal.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#  This script will be run after parameterization has completed, e.g., 
+#  use this to compile source code that has been parameterized.
+#  The container user password will be passed as the first argument,
+#  (the user ID is the second parameter)
+#  If this script is to use sudo and the sudoers for the lab
+#  does not permit nopassword, then use:
+#  echo $1 | sudo -S the-command
+#
+#  If you issue commands herein to start services, and those services
+#  have unit files prescribing their being started after the
+#  waitparam.service, then first create the flag directory that
+#  waitparam sleeps on:
+#
+#   PERMLOCKDIR=/var/labtainer/did_param
+#   echo $1 | sudo -S mkdir -p "$PERMLOCKDIR"
+#cd $HOME
+#echo "This script updates the Juiceshop database."
+
+#cd /juice-shop_7.4.1
+
+#echo $1 | sudo -S ls -l /juice-shop_7.4.1/data/juiceshop.sqlite &>> /home/ubuntu/ls.txt
+chmod 755 /update.sh
+#echo $1 | sudo -S /home/ubuntu/update.sh &>> /home/ubuntu/log.txt
+#/home/ubuntu/update.sh &>> /home/ubuntu/log.txt
+echo $1 | sudo systemctl enable juice-shop
+echo $1 | sudo systemctl start juice-shop
+echo $1 | sudo systemctl enable juice-update
+echo $1 | sudo systemctl start juice-update
+#touch /home/ubuntu/new.txt
+#rm ./update.sh 
---
+++ web-xxe/web-xxe-server/_system/update.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+
+SECRET2_VALUE=RAND_INT
+
+if [ $SECRET2_VALUE = 1 ];
+then
+	cp /vulnerabilities/displayfilev1 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 2 ];
+then
+	cp /vulnerabilities/displayfilev2 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 3 ];
+then
+	cp /vulnerabilities/displayfilev3 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 4 ];
+then
+	cp /vulnerabilities/displayfilev4 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 5 ];
+then
+	cp /vulnerabilities/displayfilev5 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 6 ];
+then
+	cp /vulnerabilities/displayfilev6 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 7 ];
+then
+	cp /vulnerabilities/displayfilev7 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 8 ];
+then
+	cp /vulnerabilities/displayfilev8 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 9 ];
+then
+	cp /vulnerabilities/displayfilev9 /displayfile
+fi
+
+if [ $SECRET2_VALUE = 10 ];
+then
+	cp /vulnerabilities/displayfilev10 /displayfile
+fi
